4be03f5135557098362017635e47dc66f247e47d
Saroj Thapa <sarose301@gmail.com>
2019-10-21 16:21:25 -0500

    Cart Entry added

--- src/main/java/edu/mum/cs/waa/project/bajargebeyaproject/controller/CartController.java
+++ src/main/java/edu/mum/cs/waa/project/bajargebeyaproject/controller/CartController.java
@@ -1,13 +1,43 @@
 package edu.mum.cs.waa.project.bajargebeyaproject.controller;
 
+import edu.mum.cs.waa.project.bajargebeyaproject.domain.Buyer;
+import edu.mum.cs.waa.project.bajargebeyaproject.domain.Cart;
+import edu.mum.cs.waa.project.bajargebeyaproject.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class CartController {
 
+    private CartService cartService;
+    @Autowired
+    public CartController(CartService cartService){
+        this.cartService = cartService;
+    }
+
+    //for shopping cart we need common model attributes like cart
+    @ModelAttribute
+    public void commonCartAttributes(Model model){
+        model.addAttribute("cart", new Cart());
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
     @GetMapping("/cart")
-    public String cart(){
+    public String showCart(Model model){
+        //for retrieving buyer(user) from session
+//        Buyer buyer = (Buyer) model.asMap().get("user");
+//        if(cartService.findByBuyer(buyer).isPresent()){
+//            model.addAttribute("cart", cartService.findByBuyer(buyer).get());
+//        }
         return "cart";
     }
+
+
 }
--- src/main/java/edu/mum/cs/waa/project/bajargebeyaproject/service/serviceImpl/CartServiceImpl.java
+++ src/main/java/edu/mum/cs/waa/project/bajargebeyaproject/service/serviceImpl/CartServiceImpl.java
@@ -1,7 +1,28 @@
 package edu.mum.cs.waa.project.bajargebeyaproject.service.serviceImpl;
 
+import edu.mum.cs.waa.project.bajargebeyaproject.domain.Buyer;
+import edu.mum.cs.waa.project.bajargebeyaproject.domain.Cart;
+import edu.mum.cs.waa.project.bajargebeyaproject.repository.CartRepo;
+import edu.mum.cs.waa.project.bajargebeyaproject.service.CartService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class CartServiceImpl {
+public class CartServiceImpl implements CartService {
+    private CartRepo cartRepo;
+    @Override
+    public Cart saveCart(Cart cart) {
+        return cartRepo.save(cart);
+    }
+
+    @Override
+    public void removeCart(Cart cart) {
+          cartRepo.delete(cart);
+    }
+
+    @Override
+    public Optional<Cart> findByBuyer(Buyer buyer) {
+        return cartRepo.findByBuyer(buyer);
+    }
 }
