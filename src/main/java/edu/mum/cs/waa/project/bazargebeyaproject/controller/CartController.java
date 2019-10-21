8f6b0076052fece7630ff6193a4debb8325168dc
Saroj Thapa <sarose301@gmail.com>
2019-10-21 10:18:59 -0500

    cart added

--- src/main/java/edu/mum/cs/waa/project/bazargebeyaproject/controller/CartController.java
+++ src/main/java/edu/mum/cs/waa/project/bazargebeyaproject/controller/CartController.java
@@ -1,4 +1,13 @@
 package edu.mum.cs.waa.project.bazargebeyaproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CartController {
+
+    @GetMapping("/product/cart")
+    public String cartPage(){
+        return "index";
+    }
 }
