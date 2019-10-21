f147714ad43cf2a789687b34b06be7403dcf1ca6
fekadu6 <fekaduw+1@gmail.com>
2019-10-21 11:04:27 -0500

    UI template skeleton added.

--- src/main/java/edu/mum/cs/waa/project/bazargebeyaproject/controller/UserController.java
+++ src/main/java/edu/mum/cs/waa/project/bazargebeyaproject/controller/UserController.java
@@ -1,4 +1,13 @@
 package edu.mum.cs.waa.project.bazargebeyaproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
