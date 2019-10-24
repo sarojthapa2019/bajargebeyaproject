d7aa534e9fc81a61cbf4b1b98291df120e4be91b
Sujiv Shrestha <sujivshrestha@yahoo.com>
2019-10-23 23:44:01 -0500

    From master at 11:43

--- src/main/java/edu/mum/cs/waa/project/bajargebeyaproject/utils/PdfUtil.java
+++ src/main/java/edu/mum/cs/waa/project/bajargebeyaproject/utils/PdfUtil.java
@@ -10,6 +10,8 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import edu.mum.cs.waa.project.bajargebeyaproject.domain.CartEntry;
+import edu.mum.cs.waa.project.bajargebeyaproject.domain.Receipt;
+import edu.mum.cs.waa.project.bajargebeyaproject.domain.ReceiptEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,16 +21,16 @@ import java.util.List;
 public class PdfUtil {
     private static final Logger logger = LoggerFactory.getLogger(PdfUtil.class);
 
-    public static ByteArrayInputStream CartEntries(List<CartEntry> cartEntries) {
-
+    public static ByteArrayInputStream ReceiptEntries(Receipt receipt) {
+        List<ReceiptEntry> receiptEntries = receipt.getReceiptEntries();
         Document document = new Document();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         try {
-
-            PdfPTable table = new PdfPTable(3);
-            table.setWidthPercentage(60);
-            table.setWidths(new int[]{1, 3, 3});
+//            |---id---|---ProductName---|---Qty---|---Rate---|---Amt---|
+            PdfPTable table = new PdfPTable(5);
+            table.setWidthPercentage(80);
+            table.setWidths(new int[]{1, 7, 3, 4, 5});
 
             Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
 
@@ -53,27 +55,32 @@ public class PdfUtil {
             hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(hcell);
 
-            for (CartEntry cartEntry : cartEntries) {
+            for (ReceiptEntry receiptEntry : receiptEntries) {
 
                 PdfPCell cell;
 
-                cell = new PdfPCell(new Phrase(cartEntry.getId().toString()));
+                cell = new PdfPCell(new Phrase(receiptEntry.getId().toString()));
                 cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
                 cell.setHorizontalAlignment(Element.ALIGN_CENTER);
                 table.addCell(cell);
 
-                cell = new PdfPCell(new Phrase(cartEntry.getProduct().getName()));
+                cell = new PdfPCell(new Phrase(receiptEntry.getProductName()));
                 cell.setPaddingLeft(5);
                 cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
                 cell.setHorizontalAlignment(Element.ALIGN_LEFT);
                 table.addCell(cell);
 
-                cell = new PdfPCell(new Phrase(String.valueOf(cartEntry.getQuantity())));
+                cell = new PdfPCell(new Phrase(String.valueOf(receiptEntry.getQuantity())));
                 cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
                 cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
                 cell.setPaddingRight(5);
                 table.addCell(cell);
             }
+
+            PdfPCell cell2 = new PdfPCell(new Phrase(Double.toString(receipt.getTotal())));
+            cell2.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell2);
 
             PdfWriter.getInstance(document, out);
             document.open();
